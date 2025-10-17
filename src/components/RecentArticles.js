b4c9155fd83b39944f72f340617d8a6e46fd1db8
Edmand <edmand66@hotmail.com>
2025-10-17 15:44:10 +0800

    feat(index): Added Recent Articles Section

--- src/components/RecentArticles.js
+++ src/components/RecentArticles.js
@@ -1,3 +1,103 @@
+// src/components/RecentArticles.js
 import React from "react";
-const RecentArticles = () => <div>Recent Articles Section</div>;
+
+// Array of recent articles, can be easily updated by copy and pasting and filling in the information
+const articles = [
+  {
+    title: "How to get your dream property for the best price?",
+    date: "July 30, 2014",
+    comments: 2,
+    tags: "Properties, Prices, best deals",
+    img: "http://placehold.it/766x515",
+    link: "#",
+    icon: "fa-file-text",
+    description:
+      "Sed rutrum urna id tellus euismod gravida. Praesent placerat, mauris ac pellentesque fringilla, tortor libero condimen.",
+  },
+  {
+    title: "7 tips to get the best mortgage.",
+    date: "July 24, 2014",
+    comments: 4,
+    tags: "Tips, Mortgage",
+    img: "http://placehold.it/766x515",
+    link: "#",
+    icon: "fa-film",
+    description:
+      "Sed rutrum urna id tellus euismod gravida. Praesent placerat, mauris ac pellentesque fringilla, tortor libero condimen.",
+  },
+  {
+    title: "House, location or price: What's the most important factor?",
+    date: "July 05, 2014",
+    comments: 1,
+    tags: "Location, Price, House",
+    img: "http://placehold.it/766x515",
+    link: "#",
+    icon: "fa-file-text",
+    description:
+      "Sed rutrum urna id tellus euismod gravida. Praesent placerat, mauris ac pellentesque fringilla, tortor libero condimen.",
+  },
+];
+
+// Component for displaying a single article card
+const ArticleCard = ({ article }) => (
+  <div className="item col-md-4">
+    <div className="image">
+      {/* "Read More" button linking to full article */}
+      <a href={article.link}>
+        <span className="btn btn-default">
+          <i className={`fa ${article.icon}`}></i> Read More
+        </span>
+      </a>
+      {/* Article image */}
+      <img src={article.img} alt={article.title} />
+    </div>
+
+    {/* Small icon tag for article type */}
+    <div className="tag">
+      <i className={`fa ${article.icon}`}></i>
+    </div>
+
+    {/* Article information */}
+    <div className="info-blog">
+      <ul className="top-info">
+        <li>
+          <i className="fa fa-calendar"></i> {article.date}
+        </li>
+        <li>
+          <i className="fa fa-comments-o"></i> {article.comments}
+        </li>
+        <li>
+          <i className="fa fa-tags"></i> {article.tags}
+        </li>
+      </ul>
+
+      {/* Article title */}
+      <h3>
+        <a href={article.link}>{article.title}</a>
+      </h3>
+
+      {/* Short description of the article */}
+      <p>{article.description}</p>
+    </div>
+  </div>
+);
+
+// Main recent articles component
+// Maps over the articles array and renders an article card for each article
+const RecentArticles = () => (
+  <div>
+    <h1 className="section-title">Recent Articles</h1>
+    <div className="grid-style1">
+      {articles.map((article, index) => (
+        <ArticleCard key={index} article={article} />
+      ))}
+    </div>
+    <div className="center">
+      <a href="#" className="btn btn-default-color">
+        View All News
+      </a>
+    </div>
+  </div>
+);
+
 export default RecentArticles;
