de0f8c3e361cfe163cfa081e296296f2170a4697
Edmand <edmand66@hotmail.com>
2025-10-17 23:14:36 +0800

    wip(index): Added js files from static version and linked them to index.html, also updated the header and top bar to match the static site and changed the index page to look almost similar to the static site

--- src/components/header.js
+++ src/components/header.js
@@ -1,61 +1,118 @@
-// src/components/header.js
 import React from "react";
 
 const Header = ({ setPage }) => {
   return (
-    <header>
-      {/* Top Bar */}
+    <header id="header">
+      {/* TOP BAR */}
       <div id="top-bar">
-        <ul id="top-buttons">
-          <li>
-            <button onClick={() => alert("Login clicked")}>
-              <i className="fa fa-sign-in"></i> Login
-            </button>
-          </li>
-          <li>
-            <button onClick={() => alert("Register clicked")}>
-              <i className="fa fa-pencil-square-o"></i> Register
-            </button>
-          </li>
-          <li className="divider"></li>
-          <li>
-            <div className="language-switcher">
-              <span><i className="fa fa-globe"></i> English</span>
-              <ul>
-                <li><button>Deutsch</button></li>
-                <li><button>Español</button></li>
-                <li><button>Français</button></li>
-                <li><button>Português</button></li>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <ul id="top-buttons">
+                <li>
+                  <a href="#" onClick={(e) => { e.preventDefault(); alert("Login clicked"); }}>
+                    <i className="fa fa-sign-in"></i> Login
+                  </a>
+                </li>
+                <li>
+                  <a href="#" onClick={(e) => { e.preventDefault(); alert("Register clicked"); }}>
+                    <i className="fa fa-pencil-square-o"></i> Register
+                  </a>
+                </li>
+                <li className="divider"></li>
+                <li>
+                  <div className="language-switcher">
+                    <span><i className="fa fa-globe"></i> English</span>
+                    <ul>
+                      <li><a href="#" onClick={(e) => e.preventDefault()}>Deutsch</a></li>
+                      <li><a href="#" onClick={(e) => e.preventDefault()}>Español</a></li>
+                      <li><a href="#" onClick={(e) => e.preventDefault()}>Français</a></li>
+                      <li><a href="#" onClick={(e) => e.preventDefault()}>Português</a></li>
+                    </ul>
+                  </div>
+                </li>
+
+                {/* Optional search box (kept commented out for now because its commented out on the original code)
+                <li>
+                  <form id="site-search">
+                    <span><i className="fa fa-search"></i></span>
+                    <input type="text" name="q" placeholder="Search" />
+                  </form>
+                </li>
+                */}
               </ul>
             </div>
-          </li>
-        </ul>
+          </div>
+        </div>
       </div>
 
-      {/* Main Header */}
-      <h1>One Ring Rentals</h1>
-      <nav>
-        <ul>
-          <li>
-            <button onClick={() => setPage("home")}>Find a Rental</button>
-          </li>
-          <li>
-            <button onClick={() => setPage("list")}>List your rental</button>
-          </li>
-          <li>
-            <button onClick={() => setPage("guides")}>Travel Guides</button>
-          </li>
-          <li>
-            <button onClick={() => setPage("regions")}>Regions</button>
-          </li>
-          <li>
-            <button onClick={() => setPage("news")}>News</button>
-          </li>
-          <li>
-            <button onClick={() => setPage("contact")}>Contact</button>
-          </li>
-        </ul>
-      </nav>
+      <div id="nav-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              {/* Logo */}
+              <a href="index.html" className="nav-logo">
+                <img src="images/logo.png" alt="One Ring Rentals" />
+              </a>
+
+              {/* Search Bar */}
+              <div id="sb-search" className="sb-search">
+                <form>
+                  <input
+                    className="sb-search-input"
+                    placeholder="Search..."
+                    type="text"
+                    name="search"
+                    id="search"
+                  />
+                  <input className="sb-search-submit" type="submit" value="" />
+                  <i className="fa fa-search sb-icon-search"></i>
+                </form>
+              </div>
+
+              {/* MAIN MENU */}
+              <nav className="navbar">
+                <button id="nav-mobile-btn">
+                  <i className="fa fa-bars"></i>
+                </button>
+
+                <ul className="nav navbar-nav">
+                  <li>
+                    <a href="index.html" onClick={(e) => e.preventDefault() && setPage("home")}>
+                      Find a Rental
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" onClick={(e) => e.preventDefault() && setPage("list")}>
+                      List your rental
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" onClick={(e) => e.preventDefault() && setPage("guides")}>
+                      Travel Guides
+                    </a>
+                  </li>
+                  <li>
+                    <a href="regions.html" onClick={(e) => e.preventDefault() && setPage("regions")}>
+                      Regions
+                    </a>
+                  </li>
+                  <li>
+                    <a href="news.html" onClick={(e) => e.preventDefault() && setPage("news")}>
+                      News
+                    </a>
+                  </li>
+                  <li>
+                    <a href="contact.html" onClick={(e) => e.preventDefault() && setPage("contact")}>
+                      Contact
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
--- src/pages/home.js
+++ src/pages/home.js
@@ -6,17 +6,169 @@ import Sidebar from "../components/Sidebar";
 
 const Home = () => {
   return (
-    <div className="home-page container">
-      <div className="row">
-        {/* Main Content Area */}
-        <div className="main col-sm-8">
-          <FeaturedProperties />
-          <PopularRegions />
-          <RecentArticles />
+    <div id="content">
+      {/* === BEGIN HOME SLIDER SECTION === */}
+      <div
+        id="carousel-example-generic"
+        className="carousel slide"
+        data-ride="carousel"
+      >
+        {/* Wrapper for slides */}
+        <div className="carousel-inner" role="listbox">
+          <div
+            className="item active"
+            id="slide1"
+            style={{
+              background:
+                "url(http://placehold.it/1920x605) no-repeat left center",
+              backgroundSize: "cover",
+            }}
+          >
+            <div className="carousel-caption">
+              <div className="caption sfr slider-title">Breathtaking views</div>
+              <div className="caption sfl slider-subtitle">
+                Relaxation in the Bay of Belfalas
+              </div>
+              <a href="#" className="caption sfb btn btn-default btn-lg">
+                Learn More
+              </a>
+            </div>
+          </div>
+
+          <div
+            className="item"
+            id="slide2"
+            style={{
+              background:
+                "url(http://placehold.it/1920x605) no-repeat left center",
+              backgroundSize: "cover",
+            }}
+          >
+            <div className="carousel-caption">
+              <div className="caption sfr slider-title">The simple life</div>
+              <div className="caption sfl slider-subtitle">
+                Lush gardens in Mordor
+              </div>
+              <a href="#" className="caption sfb btn btn-default btn-lg">
+                Learn More
+              </a>
+            </div>
+          </div>
         </div>
 
-        {/* Sidebar Area */}
-        <Sidebar />
+        {/* Blue Filter */}
+        <div id="home-search-section"></div>
+
+        {/* Controls */}
+        <a
+          className="left carousel-control"
+          href="#carousel-example-generic"
+          role="button"
+          data-slide="prev"
+        >
+          <span className="glyphicon glyphicon-chevron-left"></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a
+          className="right carousel-control"
+          href="#carousel-example-generic"
+          role="button"
+          data-slide="next"
+        >
+          <span className="glyphicon glyphicon-chevron-right"></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+      {/* === END HOME SLIDER SECTION === */}
+
+      {/* === BEGIN HOME ADVANCED SEARCH === */}
+      <div id="home-advanced-search" className="open">
+        <div id="opensearch"></div>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <form>
+                {/* Arrive On */}
+                <div className="form-group">
+                  <div className="form-control-small">
+                    <div
+                      className="input-group date chzn-container"
+                      data-datepicker
+                    >
+                      <input
+                        placeholder="Arrive on..."
+                        type="text"
+                        className="form-control"
+                        data-date-format="DD/MM/YYYY"
+                      />
+                      <span className="input-group-addon">
+                        <span className="glyphicon glyphicon-calendar"></span>
+                      </span>
+                    </div>
+                  </div>
+
+                  {/* Stay */}
+                  <div className="form-control-small">
+                    <select
+                      id="search_status"
+                      name="search_status"
+                      data-placeholder="Stay..."
+                    >
+                      <option value=""></option>
+                      {Array.from({ length: 14 }, (_, i) => (
+                        <option key={i + 1} value={i + 1}>
+                          {i + 1} Night{i + 1 > 1 ? "s" : ""}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+
+                  {/* Bedroom */}
+                  <div className="form-control-small">
+                    <select
+                      id="search_bedrooms"
+                      name="search_bedrooms"
+                      data-placeholder="Bedrooms"
+                    >
+                      <option value=""></option>
+                      {[0, 1, 2, 3, 4, 5].map((n) => (
+                        <option key={n} value={n}>
+                          {n === 5 ? "5+" : n}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+
+                  <div className="form-control-large">
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="location"
+                      placeholder="City, State, Country, etc..."
+                    />
+                  </div>
+
+                  <button type="submit" className="btn btn-fullcolor">
+                    Search
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* === END HOME ADVANCED SEARCH === */}
+
+      {/* === MAIN CONTENT AREA === */}
+      <div className="container">
+        <div className="row">
+          <div className="main col-sm-8">
+            <FeaturedProperties />
+            <PopularRegions />
+            <RecentArticles />
+          </div>
+          <Sidebar />
+        </div>
       </div>
     </div>
   );
