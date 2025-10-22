7ccb41f15d112d9ae3e0482dc0ffcc25de956c97
Edmand <edmand66@hotmail.com>
2025-10-22 12:41:19 +0800

    feat(regions): Updated regions layout to include images, also styled to ensure consistency in sizes

--- src/pages/Regions.js
+++ src/pages/Regions.js
@@ -1,24 +1,63 @@
+// src/components/Regions.js
 import React from "react";
 
-// Array of data for each region - for some reason on both the static site and react, the regions are not showing
+// Array of data for each region
 const regions = [
-  { name: "Sydney", location: "New South Wales", link: "#" },
-  { name: "Melbourne", location: "Victoria", link: "#" },
-  { name: "Brisbane", location: "Queensland", link: "#" },
-  { name: "Perth", location: "Western Australia", link: "#" },
-  { name: "Adelaide", location: "South Australia", link: "#" },
-  { name: "Hobart", location: "Tasmania", link: "#" },
+  {
+    name: "Sydney",
+    location: "New South Wales",
+    img: "/images/patterns/pattern1.jpg",
+    link: "#",
+  },
+  {
+    name: "Melbourne",
+    location: "Victoria",
+    img: "/images/patterns/pattern2.jpg",
+    link: "#",
+  },
+  {
+    name: "Brisbane",
+    location: "Queensland",
+    img: "/images/patterns/pattern3.jpg",
+    link: "#",
+  },
+  {
+    name: "Perth",
+    location: "Western Australia",
+    img: "/images/patterns/pattern4.jpg",
+    link: "#",
+  },
+  {
+    name: "Adelaide",
+    location: "South Australia",
+    img: "/images/patterns/pattern5.jpg",
+    link: "#",
+  },
+  {
+    name: "Hobart",
+    location: "Tasmania",
+    img: "/images/patterns/pattern6.jpg",
+    link: "#",
+  },
 ];
 
 // Component for a single region card
 const RegionCard = ({ region }) => (
   <div className="item col-md-4">
-    <div className="image">
-      <a href={region.link}>
-        <h3>{region.name}</h3>
-        <span className="location">{region.location}</span>
-      </a>
-    </div>
+    <a href={region.link}>
+      <img
+        src={region.img}
+        alt={region.name}
+        style={{
+          width: "100%",
+          height: "200px", // fixed height (all images)
+          objectFit: "cover", 
+          display: "block",
+        }}
+      />
+      <h3>{region.name}</h3>
+      <span>{region.location}</span>
+    </a>
   </div>
 );
 
